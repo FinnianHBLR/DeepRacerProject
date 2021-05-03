fae4df209b45a8498dfaf3a77fe02ec9a36be82c
Finnian-LR <finnian@fingermarkglobal.com>
2021-05-03 19:54:35 +1200

    Improved comments

--- Main.py
+++ Main.py
@@ -1,27 +1,24 @@
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
-    
-    # Read input parameters
+
+    # Inputs
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    
     steps = params['steps']
     progress = params['progress']
     
+    # Define Other Parameters
     TOTAL_NUM_STEPS = 300
     reward = 0
-    
-    # Calculate 3 markers that are at varying distances away from the center line
+
+    # Calculate 4 markers that are at varying distances away from the center line
 
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
-    
+    # Marker 4 for edge
     marker_4 = 0.75 * track_width
 
-    # Give additional reward if the car pass every 100 steps faster than expected
+    # Give additional reward if the car pass every 50 steps faster than expected
     if (steps % 50) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 50 :
         reward += 5.0
     if distance_from_center <= marker_1:
@@ -34,6 +31,4 @@ def reward_function(params):
         reward += -0.5
     else:
         reward = 1e-3  # likely crashed/ close to off track
-
-
     return float(reward)
